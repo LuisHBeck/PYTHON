ef9048d0f2af429b735a5fd3bbb182214619585c
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-04-03 23:18:19 -0300

    Update create.py

--- SENAI/PROJECTS/SEA-FLIX/create.py
+++ SENAI/PROJECTS/SEA-FLIX/create.py
@@ -10,3 +10,14 @@ def creat_db(name, age, email, plan, type, id, conexao=connection):
     cursor = conexao.cursor() #allows to execute sql commands
     cursor.execute(command) #execute the command
     conexao.commit() #edit the database (upload the changes)
+
+
+def creat_filmes(name, description, plan, classification, conexao=connection):
+    insert_filmes = f'''INSERT INTO movies(movie_name, movie_desc, movie_plan, movie_class)
+        values
+        ('{name}', '{description}', '{plan}', {classification})'''
+
+    cursor = conexao.cursor()  # allows to execute sql commands
+    cursor.execute(insert_filmes)  # execute the command
+    conexao.commit()  # edit the database (upload the changes)
+
