66f2728feddfb57ba208f3a556a15e29b6d85a23
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-04-12 16:30:19 -0300

    Update closure.py

--- UDEMY/PROJECTS/class_notes/closure.py
+++ UDEMY/PROJECTS/class_notes/closure.py
@@ -9,3 +9,25 @@ print(dobro(25))
 
 triplo = mult(3)
 print(triplo(15))
+
+#--------------------
+#--------------------
+#--------------------
+
+
+def sum(x, y):
+    return x + y
+
+
+def mult(x, y):
+    return x * y
+
+
+def over(func, x):
+    def inner(y):
+        return func(x, y)
+    return inner
+
+
+x = over(sum, 5)
+print(x(15))
